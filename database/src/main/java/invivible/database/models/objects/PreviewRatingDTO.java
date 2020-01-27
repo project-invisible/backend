9e29ba8790742f8750572d838aa6d2bab9b7344e
m.thomas <93MO1999>
2020-01-27 11:34:44 +0100

    added Dockerfile, WIP for topquestions endpoint

---
+++ database/src/main/java/invivible/database/models/objects/PreviewRatingDTO.java
@@ -0,0 +1,27 @@
+package invivible.database.models.objects;
+
+import invivible.database.models.criteria.Question;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Project:        In_Visible
+ * <p>
+ * Author:         Moritz Thomas
+ * <p>
+ * Creation date:  26.01.2020
+ * <p>
+ * <p/>
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PreviewRatingDTO {
+
+  private Float aggregatedValue;
+  private Question question;
+
+}
