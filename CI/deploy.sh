e87a41f0419bdbbf0ec1c6915ff0c648ed6cb8ac
Unknown <simon.binder.94@gmail.com>
2020-01-30 09:16:03 +0100

    updated images

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -4,16 +4,15 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   echo "--- SSH"
   ssh -o StrictHostKeyChecking=no travis@invisible.f4.htw-berlin.de "pwd  ; exit"
   echo "--- Building docker file"
-  docker build -f ../database/Dockerfile -t invisible/database .
-  docker build -f ../service-discovery/Dockerfile -t invisible/service-discovery .
-  # docker build -f ../database/Dockerfile -t database .
+  docker build -f ../database/Dockerfile -t invisibleproject/in_visible:database .
+  docker build -f ../service-discovery/Dockerfile -t invisibleproject/in_visible:service-discovery .
 
   echo "--- docker images ---"
   docker images
 
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-  docker push invisible/database
-  docker push invisible/service-discovery
+  docker push invisibleproject/in_visible:database
+  docker push invisibleproject/in_visible:service-discovery
 
   scp -o StrictHostKeyChecking=no docker-compose.yml travis@invisible.f4.htw-berlin.de:~
   
